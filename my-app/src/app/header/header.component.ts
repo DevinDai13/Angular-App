71083d0c24bd533e254bcea44aaf4f0018928404
Devin Dai <wdai@ualberta.ca>
2020-05-21 13:19:50 -0600

    updated UI

---
+++ my-app/src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class TopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
+
